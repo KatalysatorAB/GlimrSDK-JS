e50caf809ee0420c208ba5bb27a6ae59a8e19a4e
Erik Rothoff Andersson <erik.rothoff@gmail.com>
2016-07-01 09:11:11 +0200

    Fix test server to work with new URL

--- spec/server.js
+++ spec/server.js
@@ -3,7 +3,7 @@ var app = express();
 
 var fs = require("fs");
 
-app.get('/v3/iptags/:id/', function (req, res) {
+app.get('/v:version/iptags/:id/', function (req, res) {
   var file = req.params.id;
 
   var contents = fs.readFileSync(__dirname + "/../spec/mock_responses/" + file + ".json").toString();
