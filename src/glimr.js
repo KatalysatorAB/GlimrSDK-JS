6838110087e8d56311a883fc305be8bea232fb75
Erik Rothoff Andersson <erik.rothoff@gmail.com>
2016-07-01 09:12:45 +0200

    lint fixes

--- src/glimr.js
+++ src/glimr.js
@@ -17,12 +17,12 @@
   // Feature detect + local reference
   // https://mathiasbynens.be/notes/localstorage-pattern
   var storage = (function() {
-  	var uid = new Date;
+  	var uid = new Date().getTime().toString();
   	var storage;
   	var result;
   	try {
   		(storage = window.localStorage).setItem(uid, uid);
-  		result = storage.getItem(uid) == uid;
+  		result = storage.getItem(uid) === uid;
   		storage.removeItem(uid);
   		return result && storage;
   	} catch (exception) {}
