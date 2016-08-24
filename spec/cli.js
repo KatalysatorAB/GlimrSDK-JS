8c279e31cbd1896a923eb56b01c25c797559cf86
Erik Rothoff Andersson <erik.rothoff@gmail.com>
2016-08-24 13:54:16 +0200

    Add testem runner mode so the test server does not have to run separately

--- spec/cli.js
+++ spec/cli.js
@@ -1,8 +1,14 @@
 var testem = require("testem");
 
 var server = require("./server");
-console.log(__dirname + "/../testem.json");
 var api = new testem();
-api.startCI({
-  file: __dirname + "/../testem.json"
-});
+
+if (process.argv[2] === "runner") {
+  api.startDev({
+    file: __dirname + "/../testem.json"
+  });
+} else {
+  api.startCI({
+    file: __dirname + "/../testem.json"
+  });
+}
