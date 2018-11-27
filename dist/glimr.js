43ad0481b134867c41abf284419ef3589108bc3c
Błażej Arkuszewski <bla.arkusz@gmail.com>
2018-11-27 11:11:21 +0100

    storeUid function

--- dist/glimr.js
+++ dist/glimr.js
@@ -1,4 +1,4 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Glimr = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Glimr = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 "use strict";
 
 module.exports = {
@@ -34,6 +34,14 @@ GlimrEnrichment.prototype = {
       this._store("u_pos", [positionObject.latitude, positionObject.longitude].join(","));
     } else {
       throw new Error("Glimr.storePosition requires one argument, an object with a numeric .longitude and .latitude");
+    }
+  },
+
+  storeUid: function(uid) {
+    if (uid && typeof uid === 'string') {
+      this._store("uid", uid);
+    } else {
+      throw new Error("Glimr.storeUid requires one string argument");
     }
   },
 
@@ -141,7 +149,8 @@ for (i = 0; i < tagCachePublicMethods.length; i += 1) {
 }
 
 var enrichmentPublicMethods = [
-  "storePosition"
+  "storePosition",
+  "storeUid"
 ];
 
 for (i = 0; i < enrichmentPublicMethods.length; i += 1) {
